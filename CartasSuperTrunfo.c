ff3e308985cc5f676531887ee6cfe4cd0a6f53ab
Inês Santos <inessantosdev15@gmail.com>
2025-02-23 13:01:16 +0000

    criação das variaveis da seginda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,13 +2,13 @@
 
 int main() {
 
-    char estado;
-    char codigoCarta[5];
-    char nomeCidade[10];
-    int populacao;
-    float area;
-    float pib;
-    int pontosTuristicos;
+    char estado1, estado2;
+    char codigoCarta1[5], codigoCarta2[5];
+    char nomeCidade1[10], nomeCidade2[10];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosTuristicos1, pontosTuristicos2;
 
     printf("------------------------------------\n");
     printf("Bem-vindos ao Super Trunfo: Cidades! \n");
@@ -18,17 +18,6 @@ int main() {
     "por meio de escolhas de características de cada carta\n\n");
     printf("Para jogar, cadastre os dados de suas cartas.\n"
     "Incialmente, cada jogador incia com duas cartas.\n\n");
-
-    printf("Primeira carta \n\n");
-    printf("informe a letra representante do estado:");
-    scanf("%c", &estado);
-
-    printf("Informe o código da carta:");
-    scanf("%s", &codigoCarta);
-
     
-
-    
-
     return 0;
 }
