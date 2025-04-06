ef18c668cf9d4da49964ad0cc5be18ea96f0b7de
Inês Santos <inessantosdev15@gmail.com>
2025-04-06 19:27:09 +0000

    alteraçao de codigo e correção de bugs

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-      
     char estado1 = {0}, estado2 = {0};
     char codigoCidade1[20] = {0}, codigoCidade2[20] = {0};
     char cidade1[100], cidade2[100];
@@ -16,19 +15,11 @@ int main() {
     printf("Bem-vindos ao Super-Trunfo Cidades! \n");
     printf("------------------------------------\n\n");
 
-    printf("O jogo Super-Trunfo consiste em tomar\n"
-    "todas as cartas em jogo dos outros participantes\n"
-    "por meio de escolhas de características de cada carta\n\n");
-
-    printf("Para jogar, cadastre os dados de suas cartas.\n"
-    "Incialmente, cada jogador incia com duas cartas.\n\n");
-
     printf("Primeira carta \n\n");
 
-    printf("Escolha uma letra de A a H para definir o estado \n\n");
-
+    printf("Escolha uma letra de A a H para definir o estado \n");
     printf("Estado: ");
-    scanf("%c", &estado1);
+    scanf(" %c", &estado1);
 
     printf("Código da cidade: ");
     scanf("%s", codigoCidade1);
@@ -39,59 +30,67 @@ int main() {
     printf("População: ");
     scanf("%f", &populacaoCidade1);
 
-    printf("Area(km²): ");
+    printf("Área(km²): ");
     scanf("%f", &areaCidade1);
 
     printf("PIB: ");
     scanf("%f", &pibCidade1);
 
-    printf("Pontos turisticos: ");
+    printf("Pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
-    resultadoDensidade1 = (float) populacaoCidade1 / areaCidade1;
-    pibPerCapita1 =  pibCidade1 / populacaoCidade1;
-    
+    resultadoDensidade1 = populacaoCidade1 / areaCidade1;
+    pibPerCapita1 = pibCidade1 / populacaoCidade1;
+
     printf("Densidade: %.2f hab/km²\n", resultadoDensidade1);
     printf("PIB per Capita: %.2f Reais\n", pibPerCapita1);
-    
-    printf("\n\n");
 
-    printf("Segunda carta \n\n");
+    printf("\n\nSegunda carta \n\n");
 
-    printf("Escolha uma letra de A a H para definir o estado \n\n");
-
+    printf("Escolha uma letra de A a H para definir o estado \n");
     printf("Estado: ");
     scanf(" %c", &estado2);
-    getchar();
 
     printf("Código da cidade: ");
     scanf("%s", codigoCidade2);
 
     printf("Cidade: ");
     scanf(" %[^\n]", cidade2);
-    getchar();
 
     printf("População: ");
     scanf("%f", &populacaoCidade2);
-    getchar();
 
-    printf("Area(km²): ");
+    printf("Área(km²): ");
     scanf("%f", &areaCidade2);
-    getchar();
-    
+
     printf("PIB: ");
     scanf("%f", &pibCidade2);
-    getchar();
 
-    printf("Pontos Turisticos: ");
+    printf("Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
-    getchar();
 
-    resultadoDensidade2 = (float) populacaoCidade1 / areaCidade2;
-    pibPerCapita2 =  pibCidade2 / populacaoCidade2;
-    
-    printf("Densidade: %.2f\n", resultadoDensidade2);
+    resultadoDensidade2 = populacaoCidade2 / areaCidade2;
+    pibPerCapita2 = pibCidade2 / populacaoCidade2;
+
+    printf("Densidade: %.2f hab/km²\n", resultadoDensidade2);
     printf("PIB per Capita: %.2f Reais\n", pibPerCapita2);
+
+    // Comparação das cartas
+    printf("\n--- Comparação de cartas ---\n");
+
+    const char* populacao = populacaoCidade1 > populacaoCidade2 ? "Carta 1 Ganhou" : "Carta 2 Ganhou";
+    const char* area = areaCidade1 > areaCidade2 ? "Carta 1 Ganhou" : "Carta 2 Ganhou";
+    const char* PIB = pibCidade1 > pibCidade2 ? "Carta 1 Ganhou" : "Carta 2 Ganhou";
+    const char* pontosTuristicos = pontosTuristicos1 > pontosTuristicos2 ? "Carta 1 Ganhou" : "Carta 2 Ganhou";
+    const char* densidade = resultadoDensidade1 > resultadoDensidade2 ? "Carta 1 Ganhou" : "Carta 2 Ganhou";
+    const char* perCapita = pibPerCapita1 > pibPerCapita2 ? "Carta 1 Ganhou" : "Carta 2 Ganhou";
+
+    printf("População: %s\n", populacao);
+    printf("Área: %s\n", area);
+    printf("PIB: %s\n", PIB);
+    printf("Pontos Turísticos: %s\n", pontosTuristicos);
+    printf("Densidade: %s\n", densidade);
+    printf("PIB per Capita: %s\n", perCapita);
 
     return 0;
 }
